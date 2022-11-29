bc2e82c22d7d06eba97e12a720176d0812b3b2db
mame0124 <mame0124.rkn@gmail.com>
2022-11-29 11:31:01 +0900

    カレンダーJavaScript版作成

---
+++ 02.calendar/calendar.js
@@ -0,0 +1,24 @@
+const argv = require("minimist")(process.argv.slice(2));
+console.log(argv);
+
+let today = new Date();
+let year = argv["y"] || today.getFullYear();
+let month = argv["m"] || today.getMonth() + 1;
+
+let firstDay = new Date(year, month - 1, 1);
+let lastDay = new Date(year, month, 0);
+
+let cal = new Array(firstDay.getDay()).fill("  ");
+for (firstDay; firstDay <= lastDay; firstDay.setDate(firstDay.getDate() + 1)) {
+  if (firstDay.getDate() < 10) {
+    cal.push(" " + firstDay.getDate());
+  } else {
+    cal.push(firstDay.getDate());
+  }
+}
+
+console.log(`      ${month}月  ${year}`);
+console.log("日 月 火 水 木 金 土");
+for (let x = 0; x < 42; x += 7) {
+  console.log(cal.slice(x, x + 7).join(" "));
+}
