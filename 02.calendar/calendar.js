4954453ad8fd41667a67b143f7874e97f0949bab
mame0124 <mame0124.rkn@gmail.com>
2022-12-14 11:30:13 +0900

    変数firstDayをfirstDateに変更、変数lastDayをlastDateに変更、for文をwhile文に変更、for文内の変数xをiに変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -4,16 +4,17 @@ const today = new Date();
 const year = argv["y"] || today.getFullYear();
 const month = argv["m"] || today.getMonth() + 1;
 
-const firstDay = new Date(year, month - 1, 1);
-const lastDay = new Date(year, month, 0);
+const firstDate = new Date(year, month - 1, 1);
+const lastDate = new Date(year, month, 0);
 
-const cal = new Array(firstDay.getDay()).fill("  ");
-for (; firstDay <= lastDay; firstDay.setDate(firstDay.getDate() + 1)) {
-  cal.push(firstDay.getDate().toString().padStart(2, " "));
+const cal = new Array(firstDate.getDay()).fill("  ");
+while (firstDate <= lastDate) {
+  cal.push(firstDate.getDate().toString().padStart(2, " "));
+  firstDate.setDate(firstDate.getDate() + 1);
 }
 
 console.log(`      ${month}月  ${year}`);
 console.log("日 月 火 水 木 金 土");
-for (let x = 0; x < 42; x += 7) {
-  console.log(cal.slice(x, x + 7).join(" "));
+for (let i = 0; i < 42; i += 7) {
+  console.log(cal.slice(i, i + 7).join(" "));
 }
